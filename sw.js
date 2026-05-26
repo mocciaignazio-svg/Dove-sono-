543bf29298becaee208d0b0927c4ee1015bf75e6
mocciaignazio-svg <moccia.ignazio@gmail.com>
2026-05-26 11:39:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('orario-cache').then((cache) => {
+    return cache.addAll(['/']);
+  }));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
