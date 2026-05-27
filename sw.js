b8870889db6c06fc24d6dec200dd9bca61e1a8cd
mocciaignazio-svg <moccia.ignazio@gmail.com>
2026-05-27 09:44:03 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('orario-cache').then((cache) => {
-    return cache.addAll(['/']);
-  }));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
